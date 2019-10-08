68424ea9f30523ab8260799197c26cfb0787684d
GeneHFang <ghl234@nyu.edu>
2019-10-07 20:03:44 -0400

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,23 @@
 # Code your solution here!
+
+def run_guessing_game
+
+    num = rand(6)
+
+    numGuess = gets
+
+   
+
+    
+    if num == numGuess
+        puts "You guessed the correct number!"
+    elsif numGuess == "exit"
+        puts "Goodbye!"
+    else
+        puts "Sorry! The computer guessed #{num}."
+        puts "numGuess is #{numGuess}"
+    end
+
+
+end
+
